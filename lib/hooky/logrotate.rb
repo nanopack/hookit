0cdcadbd88cb105bf3074c1cb9f0716fbdb3e42e
Greg Linton <lintong@pagodabox.com>
2014-06-13 14:26:51 -0600

    Added logrotate support for clean rotation of logs

---
+++ lib/hooky/logrotate.rb
@@ -0,0 +1,33 @@
+module Hooky
+  module Resource
+    class Logrotate < Base
+
+      field :path
+      field :filesize
+      field :max_size
+      field :count
+
+      actions :create
+      default_action :create
+
+      def initialize(name)
+        path name unless path
+        super
+      end
+
+      def run(action)
+        case action
+        when :create
+          create!
+        end
+      end
+
+      protected
+
+      def create!
+        `logadm -w #{path} -s #{filesize ||= '10m'} -S #{max_size ||= '500m'} -C #{count ||= '10'} -N`
+      end
+
+    end
+  end
+end
