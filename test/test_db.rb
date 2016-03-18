8519ed164fb345e117b1e4d7c65255b1ed09b3c0
Braxton Huggins <braxton@pagodabox.com>
2016-03-18 11:45:07 -0600

    add more tests

---
+++ test/test_db.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'hookit/db'
+
+class DBTest < Minitest::Test
+  def test_db
+    assert_equal true, false
+  end
+end
---
+++ test/test_registry.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'hookit/registry'
+
+class RegistryTest < Minitest::Test
+  def test_registry
+    assert_equal true, false
+  end
+end
